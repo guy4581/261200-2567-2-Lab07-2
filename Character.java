4d0858eb3e9c2e333d0bf36df974a7fb8e3dee33
NotME <152592660+guy4581@users.noreply.github.com>
2025-01-29 17:12:16 +0700

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,54 @@
+public class Character {
+
+    private int health;
+    private int stamina;
+    private static final int maxHealth = 100;
+    private static final int maxStamina = 50;
+
+    public Character() {
+
+        this.health = maxHealth;
+        this.stamina = maxStamina;
+        System.out.println("Character created with 100 health and 50 stamina.");
+
+    }
+    public int getHealth(){
+        return this.health;
+    }
+
+    public int getStamina() {
+        return this.stamina;
+    }
+
+    public void  swingSword(){
+
+            this.stamina = this.stamina - 10;
+        if (this.stamina >= 0){
+
+            System.out.println("Character swings sword. Stamina is now " + getStamina() +".");
+        }else {
+            System.out.println("Your character needs to cool down.");
+        }
+
+    }
+    public void takeDamage(int damage){
+
+        this.health = this.health - damage;
+        if(this.health > 0){
+
+            System.out.println("Character takes " + damage +" damage. Health is now " + getHealth() +".");
+        }else{
+            System.out.println("Character takes " + damage +" damage. Health is now " + getHealth() +".");
+            System.out.println("Character is dead.");
+        }
+    }
+
+    public void rest(){
+
+        this.health=maxHealth;
+        this.stamina=maxStamina;
+        System.out.println("Character has rested. Health and stamina restored to maximum.");
+
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Character guy = new Character();
+        guy.swingSword();
+        guy.swingSword();
+        guy.swingSword();
+        guy.swingSword();
+        guy.swingSword();
+        guy.swingSword();
+        guy.takeDamage(30);
+        guy.takeDamage(50);
+        guy.takeDamage(20);
+        guy.rest();
+    }
+}
